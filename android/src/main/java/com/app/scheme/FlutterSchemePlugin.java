0cf41013b02c55cb1afa96ddd761ca835f9401bf
wangyu <wangyu@socialbook.io>
2023-10-16 22:04:08 +0800

    1. fix send uri parse

--- android/src/main/java/com/app/scheme/FlutterSchemePlugin.java
+++ android/src/main/java/com/app/scheme/FlutterSchemePlugin.java
@@ -1,10 +1,14 @@
 package com.app.scheme;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Looper;
 import android.os.Message;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -102,32 +106,64 @@ public class FlutterSchemePlugin implements FlutterPlugin, MethodChannel.MethodC
 
   private void handleIntent(Context context, Intent intent) {
     String action = intent.getAction();
-    if (Intent.ACTION_VIEW.equals(action) || Intent.ACTION_SEND.equals(action)) {
-      Uri schemeUri = intent.getData();
-      if(schemeUri == null){
-        return;
+    Uri schemeUri = null;
+    String path = null;
+    if(Intent.ACTION_VIEW.equals(action)) {
+      schemeUri = intent.getData();
+    } else if(Intent.ACTION_SEND.equals(action)) {
+      String type = intent.getType();
+      if(type != null && type.startsWith("image/")) {
+        schemeUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        path = getRealPathFromUri(context, schemeUri);
       }
-      try {
-        Map<String,Object> dataMap = new HashMap<>();
-        dataMap.put("scheme",schemeUri.getScheme());
-        dataMap.put("host",schemeUri.getHost());
-        dataMap.put("port",schemeUri.getPort());
-        dataMap.put("path",schemeUri.getPath());
-        dataMap.put("query",schemeUri.getQuery());
-        dataMap.put("source","android");
-        dataMap.put("dataString",intent.getDataString());
-        if(initialIntent){
-          initialScheme = dataMap;
-          initialIntent = false;
-        }
-        latestScheme = dataMap;
-        if(eventSink != null){
-          eventSink.success(dataMap);
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
+//      ClipData clipData = intent.getClipData();
+//      int itemCount = clipData.getItemCount();
+//      if(itemCount > 0) {
+//        schemeUri = clipData.getItemAt(0).getUri();
+//      }
+    }
+    if(schemeUri == null) {
+      return;
+    }
+    try {
+      Map<String,Object> dataMap = new HashMap<>();
+      dataMap.put("scheme",schemeUri.getScheme());
+      dataMap.put("host",schemeUri.getHost());
+      dataMap.put("port",schemeUri.getPort());
+      if(path == null) {
+        dataMap.put("path",schemeUri.getPath().replaceFirst("/root",""));
+        dataMap.put("dataString",intent.getDataString().replaceFirst("/root",""));
+      } else {
+        dataMap.put("path",path);
+        dataMap.put("dataString",path);
       }
+      dataMap.put("query",schemeUri.getQuery());
+      dataMap.put("source","android");
+      if(initialIntent){
+        initialScheme = dataMap;
+        initialIntent = false;
+      }
+      latestScheme = dataMap;
+      if(eventSink != null){
+        eventSink.success(dataMap);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
+  public String getRealPathFromUri(Context context, Uri contentUri) {
+    Cursor cursor = null;
+    try {
+      String[] proj = { MediaStore.Images.Media.DATA };
+      cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+      int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+      cursor.moveToFirst();
+      return cursor.getString(column_index);
+    } finally {
+      if (cursor != null) {
+        cursor.close();
+      }
     }
   }
 }
